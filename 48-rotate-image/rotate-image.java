bf1c8d67637c2aafcbc7d152fda65233f3442882
sohanjitbanik0-netizen <sohanjitbanik0@gmail.com>
2026-08-25 23:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (16.34%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        for(int i = 0; i< n; i++){
+            for(int j = i+1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //reverse each row
+        for (int i = 0; i < n; i++){
+            int left = 0;
+            int right = matrix[i].length - 1;
+
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
